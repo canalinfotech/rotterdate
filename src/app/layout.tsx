2e30e5f5091dbe7952068a4d3c2bc7ea89601682
canalinfotech <a.gupta@canalinfotech.com>
2024-10-30 22:51:46 +0100

    fix build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={geistSans.className}>{children}</body>
     </html>
   )
 }
